4b3d4350e25f80a1793519b02a10e8a21fd9b3ac
tjrgus1213 <tjrgus1213@gmail.com>
2019-10-15 00:07:53 +0900

    16236번 - 아기 상어

--- Baekjoon/아기 상어.cpp
+++ Baekjoon/아기 상어.cpp
@@ -2,95 +2,92 @@
 #include<queue>
 
 using namespace std;
-
-int plusx[4] = { -1, 0, 0, 1 };
-int plusy[4] = { 0, -1, 1, 0 };
-
-struct sha {
+struct sharkway {
 	pair<int, int> pos;
-	int cnt;
+	int len;
+	sharkway(pair<int, int> p, int l) : pos(p), len(l) {}
 };
-
 int main()
 {
-	int N;
+	int N, sharksize = 2, eat = 0;
 	int map[20][20];
 	int route[20][20] = { 0, };
+	int px[4] = { -1, 0, 0, 1 };
+	int py[4] = { 0, -1, 1, 0 };
 	pair<int, int> shark;
-	int sharksize = 2, eat = 0, totalmove = 0;;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> map[i][j];
 			if (map[i][j] == 9) {
-				shark = make_pair(i, j);
+				shark.first = i;
+				shark.second = j;
 				map[i][j] = 0;
 			}
 		}
 	}
-	sha temp;
-	bool is = true;
-	pair<int, int> temppos;
-	queue<sha> q;
-	int tempcnt, r, c, tempr, tempc, len;
-	while (is) {
-		is = false;
-		len = 1000;
-		tempr = 1000; tempc = 1000;
-		temp.pos = shark;
-		temp.cnt = 0;
-		q.push(temp);
-		route[shark.first][shark.second] = 1;
+
+	queue<sharkway> q;
+	int r, c, l;
+	int minlen, total = 0;
+	while (true) {
+		minlen = 1000;
+		q.push(sharkway(shark, 0));
 		while (!q.empty()) {
-			temppos = q.front().pos;
-			tempcnt = q.front().cnt;
-			q.pop();
-			for (int i = 0; i < 4; i++) {
-				r = temppos.first + plusx[i];
-				c = temppos.second + plusy[i];
-				if (r >= 0 && r < N && c >= 0 && c < N && route[r][c] == 0) {
-					if (map[r][c] > 0 && map[r][c] < sharksize) {
-						is = true;
-						if (len >= tempcnt) {
-							len = tempcnt;
-							if (r < tempr) {
-								tempr = r;
-								tempc = c;
-							}
-							else if (r == tempr && c < tempc) {
-								tempc = c;
-							}
-						}
+			l = q.front().len;
+			if (l > minlen) {
+				q.pop();
+				continue;
+			}
+			if (map[q.front().pos.first][q.front().pos.second] > 0 && map[q.front().pos.first][q.front().pos.second] < sharksize) {
+				if (minlen > l) {
+					minlen = l;
+					shark.first = q.front().pos.first;
+					shark.second = q.front().pos.second;
+				}
+				else if(minlen == l) {
+					if (shark.first > q.front().pos.first) {
+						shark.first = q.front().pos.first;
+						shark.second = q.front().pos.second;
 					}
-					else if (map[r][c] == 0 || map[r][c] == sharksize) {
-						if (tempcnt < len) {
-							route[r][c] = 1;
-							temp.pos = make_pair(r, c);
-							temp.cnt = tempcnt + 1;
-							q.push(temp);
-						}
-					}
+					else if (shark.first == q.front().pos.first && shark.second > q.front().pos.second)
+						shark.second = q.front().pos.second;
 				}
 			}
+			else {
+				for (int i = 0; i < 4; i++) {
+					r = q.front().pos.first + px[i];
+					c = q.front().pos.second + py[i];
+					if (r < 0 || r >= N || c < 0 || c >= N)
+						continue;
+					if (map[r][c] > sharksize)
+						continue;
+					if (route[r][c] == 1)
+						continue;
+					route[r][c] = 1;
+					q.push(sharkway(make_pair(r, c), l + 1));
+				}
+			}
+			q.pop();
 		}
 
-		if (is) {
-			while (!q.empty())
-				q.pop();
-			totalmove += len + 1;
-			shark = make_pair(tempr, tempc);
-			map[tempr][tempc] = 0;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				route[i][j] = 0;
+
+		if (minlen == 1000) {
+			break;
+		}
+		else {
+			total += minlen;
+			map[shark.first][shark.second] = 0;
 			eat++;
 			if (eat == sharksize) {
 				eat = 0;
 				sharksize++;
 			}
-			for (int i = 0; i < N; i++)
-				for (int j = 0; j < N; j++)
-					route[i][j] = 0;
 		}
 	}
-
-	cout << totalmove << endl;
+	cout << total << endl;
 	return 0;
 }
