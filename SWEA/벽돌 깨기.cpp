ea9d170d8e9190a347be1df761e06401797020c2
tjrgus1213 <tjrgus1213@gmail.com>
2019-10-15 22:08:54 +0900

    5656. [모의 SW 역량테스트] 벽돌 깨기

--- SWEA/벽돌 깨기.cpp
+++ SWEA/벽돌 깨기.cpp
@@ -1,88 +1,94 @@
 #include<iostream>
-#include<vector>
-#include<stack>
-
+#include<cstring>
 using namespace std;
 
-int function(vector<vector<int>> ary, int cnt);
+int N, W, H, tmax = 0;
+int ary[15][12][5];
+int pr[4] = { -1, 1, 0, 0 };
+int pc[4] = { 0, 0, -1, 1 };
 
-int N, W, H;
+int bomb(int layer, int r, int c);
+void func(int cnt, int brick);
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
 	cin >> T;
+
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
+		tmax = 0;
 		cin >> N >> W >> H;
-		vector<vector<int>> ary(H, vector<int>(W, 0));
-		for (int i = 0; i < H; i++)
-			for (int j = 0; j < W; j++)
-				cin >> ary[i][j];
 
-		cout << "#" << test_case << " " << function(ary, 0) << endl;
+		int br = 0;
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				cin >> ary[i][j][0];
+				if (ary[i][j][0] > 0)
+					br++;
+			}
+		}
 
+		func(0, 0);
+		br -= tmax;
+		cout << "#" << test_case << " ";
+		cout << br << endl;
 	}
 	return 0;
 }
 
-int function(vector<vector<int>> ary, int cnt) {
-	int min = 10000;
-	if (cnt == N) {
-		int brick_num = 0;
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				if (ary[i][j] != 0)
-					brick_num++;
-			}
-		}
-		return brick_num;
-	}
-	bool is = true;
+void func(int cnt, int brick) {
+	int tbrick;
 	for (int i = 0; i < W; i++) {
-		vector<vector<int>> temp_ary;
-		temp_ary.assign(ary.begin(), ary.end());
+		for (int a = 0; a < H; a++)
+			for (int b = 0; b < W; b++)
+				ary[a][b][cnt + 1] = ary[a][b][cnt];
+	
+		tbrick = brick;
 		for (int j = 0; j < H; j++) {
-			if (temp_ary[j][i] != 0) {
-				is = false;
-				stack<pair<int, int>> boom;
-				boom.push(make_pair(j, i));
-				while (!boom.empty()) {
-					int a, b;
-					a = boom.top().first;
-					b = boom.top().second;
-					boom.pop();
-					for (int k = 1 - temp_ary[a][b]; k < temp_ary[a][b]; k++) {
-						if (k + b < W && k + b > -1)
-							boom.push(make_pair(a, k + b));
-						if (k + a < H && k + a > -1)
-							boom.push(make_pair(k + a, b));
-					}
-					temp_ary[a][b] = 0;
-				}
-				for (int i2 = 0; i2 < W; i2++) {
-					int temp_pos = H - 1;
-					for (int j2 = H - 1; j2 >= 0; j2--) {
-						if (temp_ary[j2][i2] != 0) {
-							temp_ary[temp_pos][i2] = temp_ary[j2][i2];
-							temp_pos--;
-						}
-					}
-					while (temp_pos >= 0) {
-						temp_ary[temp_pos][i2] = 0;
-						temp_pos--;
-					}
-				}
-
-				int temp_num = function(temp_ary, cnt + 1);
-				if (min > temp_num)
-					min = temp_num;
+			if (ary[j][i][cnt + 1] != 0) {
+				tbrick += bomb(cnt + 1, j, i);
 				break;
 			}
 		}
+
+		for (int i2 = 0; i2 < W; i2++) {
+			int temp_pos = H - 1;
+			for (int j2 = H - 1; j2 >= 0; j2--) {
+				if (ary[j2][i2][cnt + 1] != 0) {
+					ary[temp_pos][i2][cnt + 1] = ary[j2][i2][cnt + 1];
+					temp_pos--;
+				}
+			}
+			while (temp_pos >= 0) {
+				ary[temp_pos][i2][cnt + 1] = 0;
+				temp_pos--;
+			}
+		}
+
+		if(cnt < N - 1)
+			func(cnt + 1, tbrick);
+		else {
+			if (tmax < tbrick) {
+				tmax = tbrick;
+			}
+		}
+
 	}
-	if (is)
-		return 0;
-	return min;
+}
+
+int bomb(int layer, int r, int c) {
+	int s = ary[r][c][layer];
+	ary[r][c][layer] = 0;
+	int cnt = 1;
+	for (int i = 1; i < s; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (r + pr[j] * i < 0 || r + pr[j] * i >= H || c + pc[j] * i < 0 || c + pc[j] * i >= W)
+				continue;
+			if (ary[r + pr[j] * i][c + pc[j] * i][layer] != 0)
+				cnt += bomb(layer, r + pr[j] * i, c + pc[j] * i);
+		}
+	}
+	return cnt;
 }
